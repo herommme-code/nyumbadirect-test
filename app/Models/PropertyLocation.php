21a86ef0ebe418187214734ea1786d3eafd9a8ad
Upendo Reminiscere Massawe <herommme@gmail.com>
2026-06-06 17:34:03 +0300

    Update property location and production config

--- app/Models/PropertyLocation.php
+++ app/Models/PropertyLocation.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PropertyLocation extends Model
 {
-    protected $connection = 'pgsql_locations';
-
     protected $fillable = [
         'user_id',
         'user_email',
